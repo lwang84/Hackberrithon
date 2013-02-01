8aedeec1d3477cf7887b67c894a22615ce5c586f
LingyongWang <lwang84@illinois.edu>
2013-02-01 02:01:55 -0500

    tesseract wrapper

--- Hackberrithon/Hackberrithon/HTViewController.m
+++ Hackberrithon/Hackberrithon/HTViewController.m
@@ -8,6 +8,8 @@
 
 #import "HTViewController.h"
 
+#import "Tesseract.h"
+
 @interface HTViewController ()
 
 @end
@@ -18,6 +20,13 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    Tesseract* tesseract = [[Tesseract alloc] initWithDataPath:@"Tesseract/tessdata" language:@"eng"];
+    //[tesseract setVariableValue:@"0123456789" forKey:@"tessedit_char_whitelist"];
+    [tesseract setImage:[UIImage imageNamed:@"test.jpg"]];
+    [tesseract recognize];
+    
+    NSLog(@"%@", [tesseract recognizedText]);
 }
 
 - (void)didReceiveMemoryWarning
